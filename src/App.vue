0b739193bcac21fbca49185f5d10090ec62b3f9f
aivane <Thannapat.Kham@mail.kmutt.ac.th>
2025-02-14 10:12:47 +0700

    ssd
    ss

--- src/App.vue
+++ src/App.vue
@@ -37,15 +37,15 @@ const handleClick = () => {
   <TransitionGroup name="fade">
     <div
       v-if="isClicked"
-      class="absolute inset-0 flex justify-center items-center gap-40"
+      class="absolute inset-0 flex justify-center items-center gap-60"
     >
       <img
         src="./assets/S1.png"
-        class="w-72 h-72 object-contain drop-shadow-[0_10px_30px_rgba(0,0,0,0.3)] animate-rotate-pop brightness-110"
+        class="rounded-full w-50 h-80 object-contain drop-shadow-[0_10px_30px_rgba(0,0,0,0.3)] animate-rotate-pop brightness-110"
       />
       <img
         src="./assets/S2.png"
-        class="w-72 h-72 object-contain drop-shadow-[0_10px_30px_rgba(0,0,0,0.3)] animate-rotate-pop brightness-110"
+        class="rounded-full w-50 h-80 object-contain drop-shadow-[0_10px_30px_rgba(0,0,0,0.3)] animate-rotate-pop brightness-110"
       />
     </div>
   </TransitionGroup>
